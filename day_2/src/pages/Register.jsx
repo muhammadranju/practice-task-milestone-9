ca40dfafe5c3407505770d2fdbebb9a5d04de0dd
Muhammad Ranju <mdranju23@gmail.com>
2024-11-09 01:01:56 +0600

    feat: Add register functionality using firebase auth
    - Added firebase auth
    - Added password validation
    - Show success or error message
    - Added eye icon to show password
    - Used react-icons package

--- day_2/src/pages/Register.jsx
+++ day_2/src/pages/Register.jsx
@@ -1,11 +1,50 @@
 import { NavLink } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-  const handelRegister = (e) => {
-    e.preventDefault();
-    console.log(e.target.email.value);
-  };
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [eye, setEye] = useState(true);
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+  const handelRegister = async (e) => {
+    setSuccess("");
+    setIsError("");
+    try {
+      e.preventDefault();
+      if (e.target.password.value.length === 0 || e.target.email.value === "") {
+        return setIsError("All fields are required");
+      }
+      if (e.target.password.value.length < 6) {
+        return setIsError("Password should be at least 6 characters");
+      } else if (!passwordRegex.test(e.target.password.value)) {
+        return setIsError(
+          "Password should contain at least one uppercase letter, one lowercase letter, one number and one special character"
+        );
+      }
+
+      const userData = await createUserWithEmailAndPassword(
+        auth,
+        e.target.email.value,
+        e.target.password.value
+      );
+      console.log(userData);
+      setSuccess("User Created Successfully!");
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+      setIsError(error.message);
+    }
+  };
+  const handelEyeOpen = () => {
+    setEye(!eye);
+  };
   return (
     <div className="hero  min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -17,6 +56,13 @@ const Register = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <form onSubmit={handelRegister} className="card-body">
+            {isError && (
+              <div className="alert alert-error">
+                <div>
+                  <span>{isError}</span>
+                </div>
+              </div>
+            )}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Full Name</span>
@@ -26,7 +72,6 @@ const Register = () => {
                 placeholder="Your Name"
                 name="fullName"
                 className="input input-bordered"
-                required
               />
             </div>
             <div className="form-control">
@@ -38,20 +83,29 @@ const Register = () => {
                 placeholder="email"
                 name="email"
                 className="input input-bordered"
-                required
               />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
+
+              <label className="input input-bordered flex items-center gap-2 text-gray-600">
+                <input
+                  type={eye ? "text" : "password"}
+                  name="password"
+                  placeholder="password"
+                  className="grow"
+                />
+                {eye ? (
+                  <FaEyeSlash
+                    className="cursor-pointer"
+                    onClick={handelEyeOpen}
+                  />
+                ) : (
+                  <FaEye className="cursor-pointer" onClick={handelEyeOpen} />
+                )}
+              </label>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Register</button>
@@ -64,6 +118,13 @@ const Register = () => {
                 </NavLink>
               </label>
             </div>
+            {success && (
+              <div className="alert alert-success shadow-lg">
+                <div>
+                  <span>{success}</span>
+                </div>
+              </div>
+            )}
           </form>
         </div>
       </div>
