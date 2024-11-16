3776c8b2833fc51cc98829479a9ac49f19a42516
Muhammad Ranju <mdranju23@gmail.com>
2024-11-17 00:18:15 +0600

    Refactor: Remove redundant Root layout component
    * Simplify layout structure
    * Remove unused component
    * Improve code readability
    * No functional changes
    * Update imports

--- day_4/src/router/Router.jsx
+++ day_4/src/router/Router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "../layout/Root";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -9,6 +8,7 @@ import Team from "../pages/Team";
 import PrivateRoute from "./PrivateRoute";
 import NotFound from "../layout/NotFound";
 import App from "../App";
+
 const router = createBrowserRouter([
   {
     path: "/",
